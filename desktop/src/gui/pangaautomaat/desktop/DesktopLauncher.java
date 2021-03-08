7bf5d3a6cc954a862f8963e650e8b7a409193c2d
UltraDragonDreamFeet <45874591+UltraDragonDreamFeet@users.noreply.github.com>
2021-03-08 13:24:08 +0200

    initial commit

---
+++ desktop/src/gui/pangaautomaat/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package gui.pangaautomaat.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import gui.pangaautomaat.MainClass;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MainClass(), config);
+	}
+}
